21b7e13466291faf9e171e6248409c60a1e7e5e2
FabianPoisot <fabian.poisotp@gmail.com>
2022-10-25 19:28:18 -0500

    Codigo practica puntos

---
+++ Puntos/Punto.h
@@ -0,0 +1,27 @@
+//
+// Created by V303_12 on 21/10/2022.
+//
+#include <array>
+#ifndef PUNTOS_PUNTO_H
+#define PUNTOS_PUNTO_H
+
+#include <iostream>
+#include <array>
+
+
+class Punto2D {
+public:
+    void SetPosicion(float x, float y);
+    void Trasladar(float temp1, float temp2);
+    void RotarRespectoAlOrigen(float temp1);
+    void Escalar(float temp1, float temp2);
+    float GetX();
+    float GetY();
+
+private:
+    std::array <float, 2> Posicion {};
+    std::array < std::array <float,2>, 2> Operacion {};
+
+};
+
+#endif //PUNTOS_PUNTO_H
